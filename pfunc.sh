2037727b349c5b092c84dad94e8230c79226587e
Antonio Muñiz Casado <amc1981@gmail.com>
2019-06-23 23:56:25 +0200

    CHALLANGE 4 pfunc.sh

---
+++ pfunc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+function GetFiles(){
+    TEN_FILES=`ls -1 | head -10`
+}
+function ShowFiles(){
+  GetFiles
+  LCOUNT=1
+  for FILE in GetFiles
+  do
+    echo #$LCOUNT = $FILE
+    ((COUNT++))
+  done
+}
+ShowFiles
+exit 0
